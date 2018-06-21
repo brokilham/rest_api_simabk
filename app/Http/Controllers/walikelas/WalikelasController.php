06ba8cb62ac8e4a84bb83096f70f919fc89fcc62
ilham agung <plato.ilham@gmail.com>
2018-06-21 13:40:59 +0700

    tambah file walikelas dan walimurid

---
+++ app/Http/Controllers/walikelas/WalikelasController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Http\Controllers\wallikelas;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class WalikelasController extends Controller
+{
+    /**
+     * Store a new user.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+
+   
+}    
+              
---
+++ app/Http/Controllers/walimurid/WalimuridController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Http\Controllers\walimurid;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class WalimuridController extends Controller
+{
+    /**
+     * Store a new user.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+
+   
+}    
+              
