40a12a6eb379baee0e871847a227c824623297da
brokilham <plato.ilham@gmail.com>
2018-06-25 22:45:19 +0700

    id distribusi di ubah dengan id guru, controller walikelas

--- app/Http/Controllers/walikelas/WalikelasController.php
+++ app/Http/Controllers/walikelas/WalikelasController.php
@@ -16,8 +16,6 @@ class WalikelasController extends Controller
     public function GetDataSiswaSekelas(Request $request){
         try{
 
-            $Id_WaliKelas = $request->Id_WaliKelas;
-
             $DataSiswaSeKelas = DB::select("SELECT
                                         *
                                     FROM
@@ -25,26 +23,23 @@ class WalikelasController extends Controller
                                     LEFT JOIN t_distribusi_kelas ON t_distribusi_walikelas.id_kelas = t_distribusi_kelas.id_kelas
                                     LEFT JOIN mstr_siswas ON t_distribusi_kelas.id_siswa = mstr_siswas.id
                                     WHERE
-                                        t_distribusi_walikelas.id = :id_distribusi_walikelas",["id_distribusi_walikelas" => $request->Id_WaliKelas]);         
+                                        t_distribusi_walikelas.id_guru = :id_distribusi_walikelas",["id_distribusi_walikelas" => $request->Id_WaliKelas]);         
 
             return response()->json(['status' => 'S', 'data' => $DataSiswaSeKelas] );
         }
-        catch(Exception $e)
-        {
+        catch(Exception $e){
             return response()->json(['status' => 'E', 'message' => $e] );
         }
     }
 
     public function GetPelanggaranByIdSiswa(Request $request){
-        try
-        {
+        try{
             $DataPelanggaranSiswa = DB::select("SELECT *
                                         FROM t_pelanggarans 
                                         WHERE id_siswa = :id_siswa",["id_siswa" => $request->Id_Siswa]);
             return response()->json(['status' => 'S', 'data' => $DataPelanggaranSiswa] );
         }
-        catch(Exception $e)
-        {
+        catch(Exception $e){
             return response()->json(['status' => 'E', 'message' => $e] );
         }
     }
@@ -52,16 +47,6 @@ class WalikelasController extends Controller
     public function GetDataCharts(Request $request){
         try
         {
-           //$Id_WaliKelas = $request->Id_WaliKelas;
-           
-            /*$TotalPelanggaranKelasSeminggu = DB::select(' select  count(*) as TotPelanggaranKelasSeminggu from  `t_pelanggaran_siswa` `pel_siswa` join `t_master_siswa` `mas_siswa` where Id_WaliKelas = :Id_WaliKelas  and  date_format(`pel_siswa`.`Created_Time`,"%d-%m-%Y") between  date_format(curdate()- interval 6 day,"%d-%m-%Y") and date_format(curdate(),"%d-%m-%Y") and  `pel_siswa`.Id_Siswa = `mas_siswa`.Id_Siswa' ,['Id_WaliKelas' => $Id_WaliKelas]);
-
-            $TotalPelanggaranKelasSebulan = DB::select('select  count(*) as TotPelanggaranKelasSebulan from  `t_pelanggaran_siswa` `pel_siswa` join `t_master_siswa` `mas_siswa` where Id_WaliKelas = :Id_WaliKelas  and  MONTH(Created_Time) = MONTH(curdate()) and    `pel_siswa`.Id_Siswa = `mas_siswa`.Id_Siswa' ,['Id_WaliKelas' => $Id_WaliKelas]);
-
-            $TotalBimbinganKelasSeminggu = DB::select('Select count(*) as TotBimbinganKelasSeminggu from `t_rel_konseling_bimbingan` `rel` join `t_ren_konseling_bimbingan` `ren` join `t_master_siswa` `mas_siswa`  where  `rel`.`Kode_Ren`  = `ren`.`Kode_Ren` and `ren`.Id_Siswa  =  `mas_siswa`.Id_Siswa and date_format(`Rel_waktu_janji`,"%d-%m-%Y") between  date_format(curdate()- interval 6 day,"%d-%m-%Y") and date_format(curdate(),"%d-%m-%Y") and  `rel`.Rel_Ren_Status = 0 and `mas_siswa`.Id_WaliKelas = :Id_WaliKelas' ,['Id_WaliKelas' => $Id_WaliKelas]);
-
-            $TotalBimbinganKelasSebulan = DB::select('Select count(*) as TotBimbinganKelasSebulan from `t_rel_konseling_bimbingan` `rel` join `t_ren_konseling_bimbingan` `ren` join `t_master_siswa` `mas_siswa`  where  `rel`.`Kode_Ren`  = `ren`.`Kode_Ren` and `ren`.Id_Siswa  =  `mas_siswa`.Id_Siswa and MONTH(Rel_waktu_janji) = MONTH(curdate()) and  `rel`.Rel_Ren_Status = 0 and `mas_siswa`.Id_WaliKelas = :Id_WaliKelas' ,['Id_WaliKelas' => $Id_WaliKelas]);*/
-
             $TotalPelanggaranKelasSeminggu = DB::select(" SELECT
                          count(*) as TotPelanggaranKelasSeminggu
                     FROM
@@ -70,7 +55,7 @@ class WalikelasController extends Controller
                     LEFT JOIN t_pelanggarans ON t_distribusi_kelas.id_siswa = t_pelanggarans.id_siswa 
                     WHERE
                     date_format(t_pelanggarans.created_at,'%d-%m-%Y') between  date_format(curdate()- interval 6 day,'%d-%m-%Y') AND date_format(curdate(),'%d-%m-%Y')
-                    AND  t_distribusi_walikelas.id = :id_distribusi_walikelas",["id_distribusi_walikelas" => $request->Id_WaliKelas]);    
+                    AND  t_distribusi_walikelas.id_guru = :id_distribusi_walikelas",["id_distribusi_walikelas" => $request->Id_WaliKelas]);    
                         
             $TotalPelanggaranKelasSebulan = DB::select(" SELECT
                         count(*) as TotPelanggaranKelasSebulan
@@ -80,7 +65,7 @@ class WalikelasController extends Controller
                     LEFT JOIN t_pelanggarans ON t_distribusi_kelas.id_siswa = t_pelanggarans.id_siswa 
                     WHERE
                     MONTH(t_pelanggarans.created_at) = MONTH(curdate()) AND
-                    t_distribusi_walikelas.id = :id_distribusi_walikelas",["id_distribusi_walikelas" => $request->Id_WaliKelas]);         
+                    t_distribusi_walikelas.id_guru = :id_distribusi_walikelas",["id_distribusi_walikelas" => $request->Id_WaliKelas]);         
 
 
             $TotalBimbinganKelasSeminggu = DB::select(" SELECT                                   
@@ -91,7 +76,7 @@ class WalikelasController extends Controller
                     LEFT JOIN t_bimbingans ON t_distribusi_kelas.id_siswa = t_bimbingans.id_siswa
                     WHERE
                     date_format(t_bimbingans.tgl_realisasi,'%d-%m-%Y') between  date_format(curdate()- interval 6 day,'%d-%m-%Y') AND date_format(curdate(),'%d-%m-%Y')
-                    AND t_distribusi_walikelas.id = :id_distribusi_walikelas 
+                    AND t_distribusi_walikelas.id_guru = :id_distribusi_walikelas 
                     AND t_bimbingans.status_realisasi = '1'",["id_distribusi_walikelas" => $request->Id_WaliKelas]); 
 
             $TotalBimbinganKelasSebulan = DB::select(" SELECT                                   
@@ -102,7 +87,7 @@ class WalikelasController extends Controller
                     LEFT JOIN t_bimbingans ON t_distribusi_kelas.id_siswa = t_bimbingans.id_siswa
                     WHERE
                     MONTH(t_bimbingans.tgl_realisasi) = MONTH(curdate()) 
-                    AND t_distribusi_walikelas.id = :id_distribusi_walikelas 
+                    AND t_distribusi_walikelas.id_guru = :id_distribusi_walikelas 
                     AND t_bimbingans.status_realisasi = '1'",["id_distribusi_walikelas" => $request->Id_WaliKelas]);         
 
             if (!empty($TotalPelanggaranKelasSeminggu)){ // pengecekan pelanggaran seminggu
@@ -200,7 +185,7 @@ class WalikelasController extends Controller
                                     LEFT JOIN t_pelanggarans ON t_distribusi_kelas.id_siswa = t_pelanggarans.id_siswa 
                                     LEFT JOIN mstr_siswas ON t_pelanggarans.id_siswa = mstr_siswas.id
                                     WHERE
-                                        t_distribusi_walikelas.id = :id_distribusi_walikelas",["id_distribusi_walikelas" => $request->Id_WaliKelas]);         
+                                        t_distribusi_walikelas.id_guru = :id_distribusi_walikelas",["id_distribusi_walikelas" => $request->Id_WaliKelas]);         
            
             $GetAllBimbingan = DB::select(" SELECT                                   
                                         *
@@ -210,7 +195,7 @@ class WalikelasController extends Controller
                                     LEFT JOIN t_bimbingans ON t_distribusi_kelas.id_siswa = t_bimbingans.id_siswa
                                     LEFT JOIN mstr_siswas ON t_bimbingans.id_siswa = mstr_siswas.id
                                     WHERE
-                                        t_distribusi_walikelas.id = :id_distribusi_walikelas 
+                                        t_distribusi_walikelas.id_guru = :id_distribusi_walikelas 
                                         AND t_bimbingans.status_realisasi = '1'",["id_distribusi_walikelas" => $request->Id_WaliKelas]);         
    
             return response()-> json(['status' => 'S', 'HistoryPelanggaran' => $GetAllPelanggaran,
